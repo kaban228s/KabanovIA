92d76d3847a5a19d3e8dce2403db9b47a7213b6f
Igor Kabanov <kabanovigorandreevich@gmail.com>
2023-10-30 23:12:32 +0300

    sad

---
+++ 5/5.py
@@ -0,0 +1,7 @@
+# -- coding: utf-8 --
+def main():
+    n = 0
+    while int(input('Введите число последовательности: ')) != 0:
+        n += 1
+    return n
+print(main())
---
+++ 8/8.10.2.py
@@ -0,0 +1,7 @@
+# -- coding: utf-8 --
+def reverse(s):
+    words = s.split()
+    reversed_words = ' '.join(reversed(words))
+    return reversed_words
+n = input()
+print(reverse(n))
